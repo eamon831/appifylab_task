d229ed1a1cb66e00e2314753a50dc530b428f1f1
saiful <mdsaiful.rbs@gmail.com>
2024-05-26 19:27:34 +0600

    Firbase Integrated with flutterfire CLI Tool

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import '/app/my_app.dart';
 import '/flavors/build_config.dart';
 import '/flavors/env_config.dart';
 import '/flavors/environment.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final EnvConfig devConfig = EnvConfig(
     appName: 'Appifylab Task Dev',
     baseUrl: '',
