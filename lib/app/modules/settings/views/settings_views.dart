b7cb457a495c845e4801176c7214e7b5f6a1d1f9
saiful <mdsaiful.rbs@gmail.com>
2024-05-29 21:29:23 +0600

    Label Value added and setting page updated

--- lib/app/modules/settings/views/settings_views.dart
+++ lib/app/modules/settings/views/settings_views.dart
@@ -1,3 +1,4 @@
+import 'package:appifylab_task/app/core/widget/label_value.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,6 +7,15 @@ import '/app/modules/settings/controllers/settings_controller.dart';
 
 class SettingsViews extends BaseView<SettingsController> {
   SettingsViews({super.key});
+
+  final labelValue = const LabelValue(
+    label: 'label',
+    value: 'value',
+    labelFontSize: 20,
+    valueFontSize: 18,
+    labelFontWeight: 700,
+    valueFontWeight: 600,
+  );
 
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
@@ -19,26 +29,42 @@ class SettingsViews extends BaseView<SettingsController> {
 
   @override
   Widget body(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 20),
-        Center(
-          child: Obx(
-            () => CircleAvatar(
-              radius: 100,
-              backgroundImage: Image.network(
-                controller.myUserObj.value?.photoUrl ?? '',
-                fit: BoxFit.cover,
-              ).image,
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Obx(
+        () => Column(
+          children: [
+            const SizedBox(height: 20),
+            Center(
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: Image.network(
+                  controller.myUserObj.value?.photoUrl ?? '',
+                  fit: BoxFit.cover,
+                ).image,
+              ),
             ),
-          ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: controller.changeProfileImage,
+              child: const Text('Change Profile Picture'),
+            ),
+            const SizedBox(height: 20),
+            labelValue.copyWith(
+              label: 'Name',
+              value: controller.myUserObj.value?.name ?? '',
+            ),
+            labelValue.copyWith(
+              label: 'Email',
+              value: controller.myUserObj.value?.email ?? '',
+            ),
+            labelValue.copyWith(
+              label: 'Phone',
+              value: controller.myUserObj.value?.phone ?? '',
+            ),
+          ],
         ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: controller.changeProfileImage,
-          child: const Text('Change Profile Picture'),
-        ),
-      ],
+      ),
     );
   }
 }
